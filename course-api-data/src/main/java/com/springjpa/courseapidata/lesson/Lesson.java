7b4dcd29b2a1057c1752dedd943ef4a9d6a7a717
Prodromos Nasis <prodromos.nasis@gmail.com>
2022-10-12 22:30:07 +0300

    Fixes #1

--- course-api-data/src/main/java/com/springjpa/courseapidata/lesson/Lesson.java
+++ course-api-data/src/main/java/com/springjpa/courseapidata/lesson/Lesson.java
@@ -1,6 +1,8 @@
 package com.springjpa.courseapidata.lesson;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
@@ -10,6 +12,7 @@ import com.springjpa.courseapidata.course.Course;
 public class Lesson {
 	
 	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private String id;
 	private String name;
 	private String description;
